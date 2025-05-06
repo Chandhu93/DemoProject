c94ebb1d6881e7eeee16f4040ab36053d2b7ed10
Dell <Dell@DESKTOP-GJNVS03>
2025-05-06 14:59:02 +0530

    Added pom.xml

--- DemoProject/src/test/java/pac1/CreateOrg.java
+++ DemoProject/src/test/java/pac1/CreateOrg.java
@@ -6,6 +6,17 @@ import org.testng.annotations.Test;
 public class CreateOrg {
 	@Test
 	public void createOrg() {
+		
+		String url = System.getProperty("url");
+		String browser = System.getProperty("browser");
+		String username = System.getProperty("username");
+		String password = System.getProperty("password");
+		
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
+		
 		Reporter.log("Org created", true);
 	}
 	@Test
